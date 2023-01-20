dd1611727d7da28362d216a22f32b5d35dfa8c18
ajuraszek <35744473+ajuraszek@users.noreply.github.com>
2023-01-20 20:23:47 +0100

    Initial commit

---
+++ News/News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Agnieszka Juraszek on 20/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
